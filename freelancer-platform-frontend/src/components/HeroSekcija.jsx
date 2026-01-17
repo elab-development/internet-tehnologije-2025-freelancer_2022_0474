c650d4b71797fe7a901146e4dec703ac6c5d4000
Stefan Mikic <mikicstefan1@gmail.com>
2026-01-17 14:33:47 +0100

    newsletter subscribe daje alert, hero sekcija omogucena pocetna forma, input samo estetski

--- freelancer-platform-frontend/src/components/HeroSekcija.jsx
+++ freelancer-platform-frontend/src/components/HeroSekcija.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
-import '../css/HeroSekcija.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../css/HeroSekcija.css';
 
 const HeroSekcija = () => {
+  const navigate = useNavigate();
+
+  const [selected, setSelected] = useState();
+
+  const handleSearch = () => {
+    if (selected === "freelancer") {
+      navigate('/freelancers');
+    } else if (selected === "job") {
+      navigate('/work');
+    }
+  };
+
   return (
     <div className='hero-container'>
-        <h1>Welcome to Our Job Portal</h1>
-        <p>Your gateway to exciting job opportunities</p>
-        <div className='hero-search'>
-            <div className="hero-buttons">
-            <button className='search-freelancers'>Browse Freelancers</button>
-            <button className='search-jobs'>Browse Jobs</button>
-            </div>
-            <div className="hero-input">
-            <input className='search-input' type="text" placeholder="Search..." />
-            </div>
-        </div>
-    </div>
-  )
-}
+      <h1>Welcome to Our Job Portal</h1>
+      <p>Your gateway to exciting job opportunities</p>
 
-export default HeroSekcija
+      <div className='hero-search'>
+        <div className="hero-buttons">
+          <button
+            className={selected === "freelancer" ? 'active' : ''}
+            onClick={() => setSelected("freelancer")}
+          >
+            Browse Freelancers
+          </button>
+
+          <button
+            className={selected === "job" ? 'active' : ''}
+            onClick={() => setSelected("job")}
+          >
+            Browse Jobs
+          </button>
+        </div>
+
+        <div className="hero-input">
+          <input className='search-input' type="text" placeholder="Search..."/>
+          <button className='search-btn' onClick={handleSearch}>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSekcija;
--- freelancer-platform-frontend/src/components/Newsletter.jsx
+++ freelancer-platform-frontend/src/components/Newsletter.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import '../css/Newsletter.css';
-import { MdEmail } from 'react-icons/md';
+import { MdEmail, MdRefresh } from 'react-icons/md';
 
 const Newsletter = () => {
+
+  const handleSubscribe = (e) => {
+        e.preventDefault();
+        alert("Thank you for subscribing to our newsletter!");
+        e.target.reset();
+      };
+
+
   return (
+
+      
+
     <section className="newsletter-container">
       <p className="podnaslov">OUR NEWSLETTER</p>
       <h2 className="newsletter-title">
         Subscribe to Our Newsletter for the <span>Latest Updates and Offers</span>
       </h2>
 
-      <form className="newsletter-form">
+      <form className="newsletter-form" onSubmit={handleSubscribe}>
         <div className="input-wrapper">
           <span className="email-icon"><MdEmail /></span>
           <input
