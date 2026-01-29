54b23f9a64530488778854dc6097213faa28cb6b
Stefan Mikic <mikicstefan1@gmail.com>
2026-01-29 20:31:12 +0100

    mala ispravka

--- freelancer-platform-frontend/src/components/Navbar.jsx
+++ freelancer-platform-frontend/src/components/Navbar.jsx
@@ -1,12 +1,9 @@
 import { Link, useNavigate } from "react-router-dom";
 import '../css/Navbar.css';
 import { CgProfile } from "react-icons/cg";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from "react";
 
 export default function Navbar() {
   const navigate = useNavigate();
-  const [menuOpen, setMenuOpen] = useState(false);
 
   const user = (() => {
     try {
@@ -29,37 +26,12 @@ export default function Navbar() {
         <img src="../../public/images/logo-upwork.jpg" alt="Logo" />
       </div>
 
-      {/* Hamburger za mobilni prikaz */}
-      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-        <GiHamburgerMenu size={25} color="white" />
-      </div>
-
-      <nav className={menuOpen ? "open" : ""}>
+      <nav>
         <ul id="navbar-list">
-          <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-          <li><Link to="/work" onClick={() => setMenuOpen(false)}>Work</Link></li>
-          <li><Link to="/freelancers" onClick={() => setMenuOpen(false)}>Freelancers</Link></li>
-          <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
-
-          <li className="mobile-account">
-            {user ? (
-              <div className="account-section">
-                <Link to="/profile" onClick={() => setMenuOpen(false)}>
-                  <span id="account-icon" className="profile-icon"><CgProfile /> {user.firstname}</span>
-                </Link>
-                <button id="logout-btn" onClick={handleLogout}>Logout</button>
-              </div>
-            ) : (
-              <div className="account-section">
-                <Link to="/register" onClick={() => setMenuOpen(false)}>
-                  <button id="register-btn">Register</button>
-                </Link>
-                <Link to="/login" onClick={() => setMenuOpen(false)}>
-                  <button id="login-btn">Login</button>
-                </Link>
-              </div>
-            )}
-          </li>
+          <li><Link to="/" >Home</Link></li>
+          <li><Link to="/work" >Work</Link></li>
+          <li><Link to="/freelancers" >Freelancers</Link></li>
+          <li><Link to="/contact" >Contact</Link></li>
         </ul>
       </nav>
 
