5dc48c0d3cd0fb65b401985d0dfebe7c1fc8464d
Ognjen Kalicanin <ognjenkalicanin03@gmail.com>
2026-02-24 22:37:32 +0100

    mini izmena

--- freelancer-platform-backend/models/newslettersubscriber.js
+++ freelancer-platform-backend/models/newslettersubscriber.js
@@ -12,28 +12,6 @@
  *       example: 
  *         email: john@example.com
  */
-/**
- * @swagger
- * /newsletter/subscribe:
- *   post:
- *     summary: Subscribe to newsletter
- *     tags: [Newsletter]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/NewsletterSubscriber'
- *     responses:
- *       201:
- *         description: Successfully subscribed
- *       400:
- *         description: Email is missing or invalid
- *       409:
- *         description: Email already subscribed
- *       500:
- *         description: Server error
- */
 'use strict';
 const {
   Model
